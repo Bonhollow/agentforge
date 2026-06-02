96f30bda78ed45ce22e21db9d170922ca8c3260c
Bonhollow <bonollo99@gmail.com>
2026-06-02 13:36:46 +0200

    bump formula to v0.1.2 (Continue.dev + Pi Mono adapters)

--- Formula/agentforge.rb
+++ Formula/agentforge.rb
@@ -1,8 +1,8 @@
 class Agentforge < Formula
   desc "Universal CLI to manage, sync and export AI agents across coding platforms"
   homepage "https://github.com/Bonhollow/agentforge"
-  url "https://registry.npmjs.org/@bonhollow/agentforge/-/agentforge-0.1.1.tgz"
-  sha256 "a5bb6bc9d4258bf4a98272e2bdcbeb981ff233ada0261f5c2ff0c2fd136f8cbb"
+  url "https://registry.npmjs.org/@bonhollow/agentforge/-/agentforge-0.1.2.tgz"
+  sha256 "6bd3bd224a06a0c4f21b291759016128e6fc5b2dbec03239ba18fecf07417e60"
   license "MIT"
 
   depends_on "node"
@@ -20,6 +20,6 @@ class Agentforge < Formula
   end
 
   test do
-    assert_match "0.1.1", shell_output("#{bin}/af version 2>&1")
+    assert_match "0.1.2", shell_output("#{bin}/af version 2>&1")
   end
 end
